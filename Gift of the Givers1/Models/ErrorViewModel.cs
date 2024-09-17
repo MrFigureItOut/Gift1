b7fb9e3b7948471af576dc66c3f3e2f47519ccd4
Shaquille Mohlahlo <ST10195105@rcconnect.edu.za>
2024-09-17 14:53:11 +0200

    Add project files.

---
+++ Gift of the Givers1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gift_of_the_Givers1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
